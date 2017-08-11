8ef02b37f5146bbdb16cf3689d5c6152b8f294d3
Tadej Novak <tadej@tano.si>
2017-08-11 21:39:51 +0200

    Initial runnable application

---
+++ config/compiler.pri
@@ -0,0 +1,10 @@
+#
+# Vremenar
+# Copyright (C) 2017 Tadej Novak <tadej@tano.si>
+#
+# This application is bi-licensed under the GNU General Public License
+# Version 3 or later as well as Mozilla Public License Version 2.
+# Refer to the LICENSE.md file for details.
+#
+
+CONFIG += c++14
---
+++ src/application/main.cpp
@@ -0,0 +1,24 @@
+/*
+* Vremenar
+* Copyright (C) 2017 Tadej Novak <tadej@tano.si>
+*
+* This application is bi-licensed under the GNU General Public License
+* Version 3 or later as well as Mozilla Public License Version 2.
+* Refer to the LICENSE.md file for details.
+*/
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
