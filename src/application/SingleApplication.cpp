383b9339ca6a36306ebc51d4e7e25ec8b14f2f6a
Tadej Novak <tadej@tano.si>
2019-05-04 21:22:51 +0200

    Update copyright headers

--- src/application/SingleApplication.cpp
+++ src/application/SingleApplication.cpp
@@ -5,6 +5,8 @@
 * This application is bi-licensed under the GNU General Public License
 * Version 3 or later as well as Mozilla Public License Version 2.
 * Refer to the LICENSE.md file for details.
+*
+* SPDX-License-Identifier: (GPL-3.0-or-later AND MPL-2.0)
 */
 
 #include "application/SingleApplication.h"
--- src/weather/common/models/MapLayersProxyModel.cpp
+++ src/weather/common/models/MapLayersProxyModel.cpp
@@ -1,10 +1,12 @@
 /*
 * Vremenar
-* Copyright (C) 2017 Tadej Novak <tadej@tano.si>
+* Copyright (C) 2019 Tadej Novak <tadej@tano.si>
 *
 * This application is bi-licensed under the GNU General Public License
 * Version 3 or later as well as Mozilla Public License Version 2.
 * Refer to the LICENSE.md file for details.
+*
+* SPDX-License-Identifier: (GPL-3.0-or-later AND MPL-2.0)
 */
 
 #include "weather/common/containers/MapLayer.h"
