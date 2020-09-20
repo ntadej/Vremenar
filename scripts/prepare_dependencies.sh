1df3afcbb89f13e3977369a4738aa50f7b9e5ca8
Tadej Novak <tadej@tano.si>
2020-09-20 22:08:54 +0200

    Update to Qt 5.15.1

--- scripts/prepare_dependencies.sh
+++ scripts/prepare_dependencies.sh
@@ -19,8 +19,8 @@ if [[ "${1}" == "macos" ]] || [[ "${1}" == "macOS" ]]; then
   brew install graphicsmagick imagemagick
 
   echo "Countly"
-  wget -nv -O "${LOCATION}/3rdparty/Countly/macOS/countly-sdk_20.04.2.tar.bz2" "https://dl.bintray.com/tano-si/Vremenar-Dependencies/countly/20.04.2/clang_x64/countly-sdk_20.04.2.tar.bz2"
-  tar -C "${LOCATION}/3rdparty/Countly/macOS/" -xf "${LOCATION}/3rdparty/Countly/macOS/countly-sdk_20.04.2.tar.bz2"
+  wget -nv -O "${LOCATION}/3rdparty/Countly/macOS/countly-sdk_20.04.3.tar.bz2" "https://dl.bintray.com/tano-si/Vremenar-Dependencies/countly/20.04.3/clang_x64/countly-sdk_20.04.3.tar.bz2"
+  tar -C "${LOCATION}/3rdparty/Countly/macOS/" -xf "${LOCATION}/3rdparty/Countly/macOS/countly-sdk_20.04.3.tar.bz2"
 
   echo "Sparkle"
   wget -nv -O "${LOCATION}/3rdparty/Sparkle/macOS/Sparkle-1.23.0.tar.xz" "https://github.com/sparkle-project/Sparkle/releases/download/1.23.0/Sparkle-1.23.0.tar.xz"
@@ -29,16 +29,30 @@ if [[ "${1}" == "macos" ]] || [[ "${1}" == "macOS" ]]; then
   echo "Qt Patches"
   if [[ $QT -eq 1 ]]; then
     wget -nv -O "$Qt5_Dir/plugins/platforms/libqcocoa.dylib" "https://dl.bintray.com/tano-si/Vremenar-Dependencies/qcocoa/${QT_VERSION}/libqcocoa.dylib"
+    wget -nv -O "$Qt5_Dir/plugins/geoservices/libqtgeoservices_mapboxgl.dylib" "https://dl.bintray.com/tano-si/Vremenar-Dependencies/mapbox-gl-native/${QT_VERSION}/clang_64/libqtgeoservices_mapboxgl.dylib"
   else
     echo "skipping..."
   fi
 elif [[ "${1}" == "ios" ]] || [[ "${1}" == "iOS" ]]; then
   echo "Downloading for iOS"
+  brew install ldid
+
   echo "Countly"
-  wget -nv -O "${LOCATION}/3rdparty/Countly/iOS/countly-sdk_20.04.2.tar.bz2" "https://dl.bintray.com/tano-si/Vremenar-Dependencies/countly/20.04.2/ios/countly-sdk_20.04.2.tar.bz2"
-  tar -C "${LOCATION}/3rdparty/Countly/iOS/" -xf "${LOCATION}/3rdparty/Countly/iOS/countly-sdk_20.04.2.tar.bz2"
-  wget -nv -O "${LOCATION}/3rdparty/Countly/iOSSimulator/countly-sdk_20.04.2.tar.bz2" "https://dl.bintray.com/tano-si/Vremenar-Dependencies/countly/20.04.2/iossimulator/countly-sdk_20.04.2.tar.bz2"
-  tar -C "${LOCATION}/3rdparty/Countly/iOSSimulator/" -xf "${LOCATION}/3rdparty/Countly/iOSSimulator/countly-sdk_20.04.2.tar.bz2"
+  wget -nv -O "${LOCATION}/3rdparty/Countly/iOS/countly-sdk_20.04.3.tar.bz2" "https://dl.bintray.com/tano-si/Vremenar-Dependencies/countly/20.04.3/ios/countly-sdk_20.04.3.tar.bz2"
+  tar -C "${LOCATION}/3rdparty/Countly/iOS/" -xf "${LOCATION}/3rdparty/Countly/iOS/countly-sdk_20.04.3.tar.bz2"
+  wget -nv -O "${LOCATION}/3rdparty/Countly/iOSSimulator/countly-sdk_20.04.3.tar.bz2" "https://dl.bintray.com/tano-si/Vremenar-Dependencies/countly/20.04.3/iossimulator/countly-sdk_20.04.3.tar.bz2"
+  tar -C "${LOCATION}/3rdparty/Countly/iOSSimulator/" -xf "${LOCATION}/3rdparty/Countly/iOSSimulator/countly-sdk_20.04.3.tar.bz2"
+
+  echo "Qt Patches"
+  if [[ $QT -eq 1 ]]; then
+    pushd "$Qt5_Dir" || exit 1
+    patch -p1 < "${LOCATION}/3rdparty/Xcode_12.patch"
+    popd || exit 1
+    wget -nv -O "$Qt5_Dir/plugins/geoservices/libqtgeoservices_mapboxgl.a" "https://dl.bintray.com/tano-si/Vremenar-Dependencies/mapbox-gl-native/${QT_VERSION}/ios/libqtgeoservices_mapboxgl.a"
+    wget -nv -O "$Qt5_Dir/plugins/geoservices/libqtgeoservices_mapboxgl_debug.a" "https://dl.bintray.com/tano-si/Vremenar-Dependencies/mapbox-gl-native/${QT_VERSION}/ios/libqtgeoservices_mapboxgl_debug.a"
+  else
+    echo "skipping..."
+  fi
 elif [[ "${1}" == "linux" ]] || [[ "${1}" == "Linux" ]]; then
   echo "Downloading for Linux"
   echo "Qt Patches"
