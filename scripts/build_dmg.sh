c190a7bdd4d7d3119cac4c70b1169e40e496a675
Tadej Novak <tadej@tano.si>
2021-07-19 20:34:01 +0200

    Add DMG cleanup

--- scripts/build_dmg.sh
+++ scripts/build_dmg.sh
@@ -2,6 +2,24 @@
 
 macdeployqt "$1" -qmldir="$2/src/qml/" -appstore-compliant
 
+# cleanup
+if [[ -f "$1/Contents/PlugIns/geoservices/libqtgeoservices_esri.dylib" ]]; then
+  rm "$1/Contents/PlugIns/geoservices/libqtgeoservices_esri.dylib"
+fi
+if [[ -f "$1/Contents/PlugIns/geoservices/libqtgeoservices_itemsoverlay.dylib" ]]; then
+  rm "$1/Contents/PlugIns/geoservices/libqtgeoservices_itemsoverlay.dylib"
+fi
+if [[ -f "$1/Contents/PlugIns/geoservices/libqtgeoservices_mapbox.dylib" ]]; then
+  rm "$1/Contents/PlugIns/geoservices/libqtgeoservices_mapbox.dylib"
+fi
+if [[ -f "$1/Contents/PlugIns/geoservices/libqtgeoservices_maplibre.dylib" ]]; then
+  rm "$1/Contents/PlugIns/geoservices/libqtgeoservices_maplibre.dylib"
+fi
+if [[ -f "$1/Contents/PlugIns/geoservices/libqtgeoservices_nokia.dylib" ]]; then
+  rm "$1/Contents/PlugIns/geoservices/libqtgeoservices_nokia.dylib"
+fi
+
+# dmg
 create-dmg "$1" --overwrite
 result=$?
 if [[ $result -eq 0 || $result -eq 2 ]]; then
