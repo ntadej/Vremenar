6eca73d6d355d2ed5f53ed8e8bfc2635daedad78
Tadej Novak <tadej@tano.si>
2024-08-18 09:27:29 +0200

    Minor ruby dependencies update

--- Gemfile
+++ Gemfile
@@ -2,4 +2,4 @@ source 'https://rubygems.org'
 
 gem 'fastlane', '2.222.0'
 
-gem 'rubocop', '1.65.1', require: false
+gem 'rubocop', '1.66.0', require: false
--- fastlane/actions/create_windows_package.rb
+++ fastlane/actions/create_windows_package.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop:disable List/MissingCopEnableDirective, Metrics/ClassLength, Metrics/BlockLength
+
 module Fastlane
   module Actions
     module SharedValues
